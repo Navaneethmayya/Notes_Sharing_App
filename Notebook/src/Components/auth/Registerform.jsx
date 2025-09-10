3ad4ec05fdb775e1d9b5bb5ccf08f09d7b3656a6
Navaneethmayya <navaneethgmayya@gmail.com>
2025-09-10 12:12:07 +0530

    change Login and Register
    Modified styling of Register Page and handled invalid crendentials, unauthorized users in Login page.

--- Notebook/src/Components/auth/Registerform.jsx
+++ Notebook/src/Components/auth/Registerform.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
-import axios from 'axios';
-
+import axios from "axios";
+import LoginPage from "../../Pages/LoginPage";
 
 const Registerform = () => {
   const [name, setname] = useState("");
@@ -14,92 +14,102 @@ const Registerform = () => {
   const handleSubmit = async () => {
     // toast.info(name + " " + email + " " + password + " " + type);
 
-    try{
-
-      const res=await axios.post("http://localhost:5184/api/Users/register",{
+    try {
+      const res = await axios.post("http://localhost:5184/api/Users/register", {
         Name: name,
-        Email:email,
-        Password:password,
-        Type : type,
-        Bio : bio,
+        Email: email,
+        Password: password,
+        Type: type,
+        Bio: bio,
       });
       toast.success("Registered Successfully");
-      console.log("res is",res.data);
-      toast.info("your unique Username is"+res.data.username);
-      
-    }catch(error){
-      toast.warning("unable to register    "+error);
+      console.log("res is", res.data);
+      toast.info("your unique Username is" + res.data.username);
+    } catch (error) {
+      toast.warning("unable to register    " + error);
     }
-
   };
 
   return (
-    <div>
-      <Form>
-        <FormGroup>
-          <Label for="username">Username</Label>
-          <Input
-            id="username"
-            name="username"
-            placeholder="write your username"
-            type="text"
-            value={name}
-            onChange={(e) => setname(e.target.value)}
-          />
-        </FormGroup>
+    <>
+      <div className="absolute bg-white z-10 left-100 top-1 px-3 py-1 rounded-2xl">
+        <div className="flex justify-center align-middle">
+          <h3>Register Form</h3>
+        </div>
+        <div className="flex align-middle justify-center max-w-fit">
+          <Form className="w-120 mt-10">
+            <FormGroup>
+              <Label for="username">Username</Label>
+              <Input
+                id="username"
+                name="username"
+                placeholder="write your username"
+                type="text"
+                value={name}
+                onChange={(e) => setname(e.target.value)}
+              />
+            </FormGroup>
 
-        <FormGroup>
-          <Label for="email">Email</Label>
-          <Input
-            id="email"
-            name="email"
-            placeholder="Enter Your Email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </FormGroup>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                placeholder="Enter Your Email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormGroup>
 
-        <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input
-            id="examplePassword"
-            name="password"
-            placeholder="password placeholder"
-            type="password"
-            valide={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleSelect">Select</Label>
-          <Input
-            id="exampleSelect"
-            name="select"
-            type="select"
-            value={type}
-            onChange={(e) => setType(e.target.value)} // ✅ Handle input change
-          >
-            <option value="">-- Select a Role --</option>
-            <option value="Admin">Admin</option>
-            <option value="Author">Author</option>
-            <option value="Visitor">Visitor</option>
-          </Input>
-        </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                id="examplePassword"
+                name="password"
+                placeholder="password placeholder"
+                type="password"
+                valide={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleSelect">Select</Label>
+              <Input
+                id="exampleSelect"
+                name="select"
+                type="select"
+                value={type}
+                onChange={(e) => setType(e.target.value)} // ✅ Handle input change
+              >
+                <option value="">-- Select a Role --</option>
+                <option value="Admin">Admin</option>
+                <option value="Author">Author</option>
+                <option value="Visitor">Visitor</option>
+              </Input>
+            </FormGroup>
 
-        <FormGroup>
-          <Label for="exampleText">Tell About Yourself</Label>
-          <Input id="exampleText" name="text" type="textarea" onChange={(e)=>setBio(e.target.value)} />
-        </FormGroup>
-
-        <Button onClick={handleSubmit}>Submit</Button>
-      </Form>
-    </div>
+            <FormGroup>
+              <Label for="exampleText">Tell About Yourself</Label>
+              <Input
+                id="exampleText"
+                name="text"
+                type="textarea"
+                onChange={(e) => setBio(e.target.value)}
+              />
+            </FormGroup>
+            <div className="flex justify-center">
+            <Button onClick={handleSubmit}>Submit</Button>
+            </div>
+          </Form>
+        </div>
+      </div>
+      <LoginPage/>
+    </>
   );
 };
 
 export default Registerform;
-
 
 ////////////////////// Read Me //////////////////////
 /* this is a registration component consisting of reactstrap fomr with axios based request where we have 
