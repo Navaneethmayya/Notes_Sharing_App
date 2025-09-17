ad1512908961de3c495cebd9fb2d063c3d0270e9
Deepakdhore <87926172+Deepakdhore@users.noreply.github.com>
2025-09-17 16:50:00 +0530

    Prototype 2.0
    added floating feature for nodes

--- Notebook/src/Components/GraphFolderStruture/Flow.jsx
+++ Notebook/src/Components/GraphFolderStruture/Flow.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback,useEffect, useState } from "react";
 import "./Flow.css"
 import ReactFlow, {
   MiniMap,
@@ -11,31 +11,58 @@ import ReactFlow, {
 
 import "reactflow/dist/style.css";
 import Button from "../Button/Buttons";
+import { toast } from "react-toastify";
 
 
 export default function Flow() {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [deleteBtn,setDeleteBtn] = useState(false);
   
   const [nodeId,setNodeId] =useState("");
   const [nodeLabel,setNodeLabel] =useState("");
   const [cFrom,setCFrom] =useState("");
   const [cTo,setCTo] =useState("");
 
+    useEffect(() => {
+    const interval = setInterval(() => {
+      setNodes((nds) =>
+        nds.map((n) => ({
+          ...n,
+          position: {
+            x: n.position.x + (Math.random() - 0.5) * 10, 
+            y: n.position.y + (Math.random() - 0.5) * 10, 
+
+            //   x: n.position.x +  5, 
+            // y: n.position.y +  5,
+          },
+        }))
+      );
+    }, 300); // update every 300ms
+
+    return () => clearInterval(interval);
+  }, [setNodes]);
+
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
   );
 
+  const onNodeClick=()=>{
+    setDeleteBtn(true);
+  }
   const addAgent = () => {
-    const newId = nodeId;
+
+    if(nodeId !== "")
+    {
+      const newId = nodeId.toLowerCase();
 
     const newPosition = {
       x: Math.floor(Math.random() * 200) + 100,
       y: Math.floor(Math.random() * 200) + 100,
     };
 
-   const newData = { label: nodeLabel || `Agent ${newId}` };
+   const newData = { label: nodeLabel };
 
     const newNode = {
       id: newId,
@@ -50,24 +77,31 @@ export default function Flow() {
       console.log("all nodes:", updated);
       return updated;
     });
-    const newEdge = { id: `e${cFrom}-${cTo}`, source: cFrom, target: cTo };
+    const newEdge = { id: `e${cFrom}-${cTo}`, source: cFrom.toLowerCase(), target: cTo.toLowerCase() };
     setEdges((prev = []) => {
       const updated = [...prev, newEdge];
       console.log("new edge:", newEdge);
       console.log("all edges:", updated);
       return updated;
     });
-
+    }
+    else{
+      toast.warning("enter a topic a name to add node")
+    }
+    setNodeLabel("");
+    setCFrom("");
   };
 
   return (
     <div className="flow_div">
       <ReactFlow
+        deleteKeyCode="Delete"   
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        onNodeClick={onNodeClick}
       >
         <MiniMap />
         <Controls />
